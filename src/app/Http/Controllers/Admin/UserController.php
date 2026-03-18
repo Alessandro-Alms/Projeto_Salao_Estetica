8dded0d0525b6a692d9bbcd7bf48e85b4ba80f4e
Alessandro-Alms <alessandromaia071@gmail>
2026-03-18 11:32:55 -0300

    teste

--- src/app/Http/Controllers/Admin/UserController.php
+++ src/app/Http/Controllers/Admin/UserController.php
@@ -35,7 +35,7 @@ class UserController extends Controller
 
     public function create()
     {
-        return view('admin.usuarios.criar');
+ 
     }
 
     public function store(Request $request)
