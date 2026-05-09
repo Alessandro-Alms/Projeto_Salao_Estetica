9ef3bf7d6cfc7013ae6d4603db4c1d7a7320ef1c
Alessandro-Alms <alessandromaia071@gmail>
2026-05-09 04:20:36 -0300

    admin principal railway

--- src/database/seeders/AdminUserSeeder.php
+++ src/database/seeders/AdminUserSeeder.php
@@ -10,14 +10,18 @@ class AdminUserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
-            'name' => env('ADMIN_NAME', 'Gerente Principal'),
-            'email' => env('ADMIN_EMAIL', 'gerente@salao.com'),
-            'password' => Hash::make(env('ADMIN_PASSWORD', 'senha123')),
-            'cpf' => env('ADMIN_CPF', '12345678901'),
-            'telefone' => env('ADMIN_PHONE', '85987654321'),
-            'cargo' => 'gerente',
-            'status' => 'ativo',
-        ]);
+        $email = env('ADMIN_EMAIL', 'gerente@salao.com');
+
+        User::updateOrCreate(
+            ['email' => $email],
+            [
+                'name' => env('ADMIN_NAME', 'Gerente Principal'),
+                'password' => Hash::make(env('ADMIN_PASSWORD', 'senha123')),
+                'cpf' => env('ADMIN_CPF', '12345678901'),
+                'telefone' => env('ADMIN_PHONE', '85987654321'),
+                'cargo' => 'gerente',
+                'status' => 'ativo',
+            ]
+        );
     }
 }
